6a88f19b5d65d25381d76dc326d56658b2039f40
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 00:08:42 +0100

    updated dependencies

--- dev/boot.ts
+++ dev/boot.ts
@@ -1,3 +1,6 @@
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+// wichtig, wenn man hin zu ES5 transpiliert
+
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 
