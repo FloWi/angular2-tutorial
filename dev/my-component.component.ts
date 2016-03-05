0a91b6786ca8e4404020b43489d9f8f521a96fb7
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 00:34:10 +0100

    completed unit #15

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -3,12 +3,19 @@ import {Component} from "angular2/core"
 @Component({
     selector: "my-component",
     template: `
-        Hi, I'm {{name}} and this is my very first Angular 2 component!
+        Hi, I'm <span [style.color]="inputElement.value === 'yes' ? 'orange' : ''">{{name}}</span> is my very first Angular 2 component!
         <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!!</span>
-        <br/>
-        <br/>
+        <br/> <br/>
+
         Is it awesome? Enter 'yes' to show it's awesome
+        <br/> <br/>
+
         <input type="text" #inputElement (keyup)="0">
+        <br/> <br/>
+
+        <button [disabled]="inputElement.value !== 'yes'">Only enabled, if 'yes' has been entered</button>
+
+
 
         <!--#inputElement creates a local variable &ndash;&gt; hash-tag + variableName-->
     `,
