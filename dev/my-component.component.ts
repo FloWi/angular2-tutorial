027cc75ebfaaa34dad5bdcca9318fc4b82901d09
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 00:26:44 +0100

    completed unit #14

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -3,9 +3,17 @@ import {Component} from "angular2/core"
 @Component({
     selector: "my-component",
     template: `
-        This is my component!
-    `
+        Hi, I'm {{name}} and this is my very first Angular 2 component!
+        <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!!</span>
+        <br/>
+        <br/>
+        Is it awesome? Enter 'yes' to show it's awesome
+        <input type="text" #inputElement (keyup)="0">
+
+        <!--#inputElement creates a local variable &ndash;&gt; hash-tag + variableName-->
+    `,
+    styleUrls: ["src/css/mycomponent.css"]
 })
 export class MyComponentComponent {
-
+    name = "Florian"
 }
