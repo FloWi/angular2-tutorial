31538577d5aad34bb4014d31ff4a7f57f30dbc16
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 16:18:57 +0100

    completed unit #20 - string interpolation

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,18 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-
+        <h1>Angular 2 Directives - String interpolation</h1>
+        {{'Hey'}} <br>
+        {{1 === 1}} <br>
+        {{name}} <br>
+        {{testFn()}} <br>
+        <input type="text" value="{{name}}" class="{{'redClass'}}"> <br>
     `,
 })
 export class AppComponent {
+    name = 'Florian';
 
+    testFn() {
+        return 1 === 1;
+    }
 }
