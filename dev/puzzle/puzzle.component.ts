54a850564df578ac6baf3dfec8557ad6dd7ec807
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 16:00:30 +0100

    completed unit #19 - game done (with lots of copy&paste

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core'
+import {OnInit} from "angular2/core";
 
 @Component({
     selector: 'flwi-puzzle',
@@ -14,31 +15,41 @@ import {Component} from 'angular2/core'
         <section
         [ngClass]="{
             puzzle: true,
-            solved: switch1.value == switch1Expected &&
-                    switch2.value == switch2Expected &&
-                    switch3.value == switch3Expected &&
-                    switch4.value == switch4Expected
+            solved: switch1.value == switch1Expected && switch2.value == switch2Expected && switch3.value == switch3Expected && switch4.value == switch4Expected
         }"
         [ngStyle]="{display: name.value === '' ? 'none' : 'block'}"
         >
-            <h2>The Puzzle</h2>
+            <h2>
+            The Puzzle |
+            {{switch1.value == switch1Expected && switch2.value == switch2Expected && switch3.value == switch3Expected && switch4.value == switch4Expected ? 'SOLVED' : 'NOT SOLVED'}}
+            </h2>
             <p>Ok, welcome <span class="name">{{name.value}}</span></p>
             <br>
             Swtitch 1:
-            <input type="text" #switch1><br>
+            <input type="number" #switch1 (keyup)="0" value="-1" max="1" min="-1"><br>
             Swtitch 2:
-            <input type="text" #switch2><br>
+            <input type="number" #switch2 (keyup)="0" value="-1" max="1" min="-1"><br>
             Swtitch 3:
-            <input type="text" #switch3><br>
+            <input type="number" #switch3 (keyup)="0" value="-1" max="1" min="-1"><br>
             Swtitch 4:
-            <input type="text" #switch4><br>
+            <input type="number" #switch4 (keyup)="0" value="-1" max="1" min="-1"><br>
         </section>
-        <h2>Congratulations {{name.value}}, you did it!</h2>
+        <h2 [hidden]="!(switch1.value == switch1Expected && switch2.value == switch2Expected && switch3.value == switch3Expected && switch4.value == switch4Expected)">Congratulations {{name.value}}, you did it!</h2>
     `
 })
-export class PuzzleComponent {
+export class PuzzleComponent implements OnInit {
     switch1Expected: number;
     switch2Expected: number;
     switch3Expected: number;
     switch4Expected: number;
+
+
+    ngOnInit():any {
+
+        this.switch1Expected = Math.round(Math.random());
+        this.switch2Expected = Math.round(Math.random());
+        this.switch3Expected = Math.round(Math.random());
+        this.switch4Expected = Math.round(Math.random());
+        console.log(this.switch1Expected, this.switch2Expected, this.switch3Expected, this.switch4Expected);
+    }
 }
