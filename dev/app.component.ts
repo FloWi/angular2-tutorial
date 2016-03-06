ce077899a1296d152425dda96997e1d34523166e
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 13:55:00 +0100

    cleared out playground for little game-project

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,14 +1,10 @@
 import {Component} from 'angular2/core';
-import {MyComponentComponent} from "./my-component.component";
 
 @Component({
-    selector: 'my-app',
-    directives: [MyComponentComponent], //very important - otherwise nothing happens ;-)
+    selector: 'app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
 
-        <h2>Now comes the 2nd component</h2>
-        <my-component></my-component>
     `,
 })
 export class AppComponent {
