c67d4550c1486fd4458ba71e6b0088c0a6245972
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 13:53:32 +0100

    completed unit #16

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -1,9 +1,11 @@
 import {Component} from "angular2/core"
+import {TestComponent} from "./test.component";
+import {OnInit} from "angular2/core";
 
 @Component({
     selector: "my-component",
     template: `
-        Hi, I'm <span [style.color]="inputElement.value === 'yes' ? 'orange' : ''">{{name}}</span> is my very first Angular 2 component!
+        Hi, I'm <span [style.color]="inputElement.value === 'yes' ? 'orange' : ''">{{name}}</span>. This is my very first Angular 2 component!
         <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!!</span>
         <br/> <br/>
 
@@ -11,16 +13,23 @@ import {Component} from "angular2/core"
         <br/> <br/>
 
         <input type="text" #inputElement (keyup)="0">
+        <!--#inputElement creates a local variable &ndash;&gt; hash-tag + variableName-->
         <br/> <br/>
 
         <button [disabled]="inputElement.value !== 'yes'">Only enabled, if 'yes' has been entered</button>
-
-
-
-        <!--#inputElement creates a local variable &ndash;&gt; hash-tag + variableName-->
+        <br/> <br/>
+        <test></test>
     `,
-    styleUrls: ["src/css/mycomponent.css"]
+    styleUrls: ["src/css/mycomponent.css"],
+    directives: [TestComponent]
 })
-export class MyComponentComponent {
-    name = "Florian"
+export class MyComponentComponent implements OnInit {
+    name:string;
+
+    ngOnInit():any {
+
+        this.name = "Florian Witteler"
+
+
+    }
 }
