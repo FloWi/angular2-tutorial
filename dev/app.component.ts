801e70317dd326f2fd755b291686bd92de43fe96
Florian Witteler <florian_witteler@gmx.de>
2016-03-06 16:30:46 +0100

    completed unit #21 - property binding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -9,6 +9,12 @@ import {Component} from 'angular2/core';
         {{name}} <br>
         {{testFn()}} <br>
         <input type="text" value="{{name}}" class="{{'redClass'}}"> <br>
+
+        <h1>Angular 2 Directives - Property binding</h1>
+        bound to an angularjs introduced default property (as in 'value') or directive (as in ngClass) <br>
+        directives of angularjs are prefixed with 'ng' <br>
+        <input type="text" [value]="name" [ngClass]="{redClass: true}"> <br>
+
     `,
 })
 export class AppComponent {
