60c19be1cd0820fb5f123537d8d9cd1e4dab17d5
Florian Witteler <florian_witteler@gmx.de>
2016-03-07 21:41:29 +0100

    starting all over for new project

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,35 +1,12 @@
 import {Component} from 'angular2/core';
-import {PropertyBindingComponent} from "./property-binding.component";
 
 @Component({
     selector: 'app',
     template: `
-        <section class="parent">
-            <h2>This is the parent component</h2>
-            <h4>Please enter your name</h4>
-            <input type="text" [(ngModel)]="name">
-            <br><br>
-            <pre>{{name}}</pre>
-            <p>The hobbies of the child-component are {{hobbies}}.</p>
-            <br><br>
-            <section class="child"
-            [ngStyle]="{display: name == null || name === '' ? 'none' : 'block'}"
-            >
-                <flwi-property-binding
-                [myName]="name"
-                [myAge]="29"
-                [myRealAge]="34"
-                (hobbiesChanged)="hobbies = $event"
-                ></flwi-property-binding>
+        <h1>Angular 2 Boilerplate</h1>
 
-            </section>
-
-        </section>
     `,
-    directives: [PropertyBindingComponent]
 })
 export class AppComponent {
-
-    hobbies = '';
 
 }
