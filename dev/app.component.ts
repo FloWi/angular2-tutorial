3c5f40b337da4281515ada914bc702289193e819
Florian Witteler <florian_witteler@gmx.de>
2016-03-07 22:04:41 +0100

    completed unit #30 - project input component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,32 @@
 import {Component} from 'angular2/core';
+import {InputComponent} from "./bindings/input.component";
 
 @Component({
     selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
+        <div class="container">
+            <flwi-input (submitted)="onSubmit($event)"></flwi-input>
+        </div>
+
+        <div class="container">
+            <h3>submitted value</h3>
+            <pre>{{objString}}</pre>
+        </div>
+
+        <div class="container">
+            ...
+        </div>
 
     `,
+    directives: [InputComponent]
 })
 export class AppComponent {
 
+    myself = {name: '', age: ''};
+    confirmedMyself = {name: '', age: ''};
+    objString = '';
+
+    onSubmit(obj) {
+        this.objString = JSON.stringify(obj);
+    }
 }
